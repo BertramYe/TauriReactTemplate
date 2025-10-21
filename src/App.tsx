214f494bf0a9f6b35cd2792c96b1aba16f247c0c
BertramYe <bertramyerik@gmail>
2025-10-21 22:56:23 +0800

    the first release version is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
+import './main.scss';
 import ErrorBoundary from '@/src/components/ErrorBoundary'
 import Transition from '@/src/components/Transition'
 import { RouterProvider } from "react-router-dom";
 import router from '@/src/routers';
-import './main.scss'
 
 const App: React.FunctionComponent = () => {
   return (
     <ErrorBoundary>
-        <React.Suspense
-          fallback={(
-            <Transition/>
-          )}
-        >
+        <React.Suspense fallback={ <Transition/> } >
           <RouterProvider router={router} >
           </RouterProvider>   
         </React.Suspense>
